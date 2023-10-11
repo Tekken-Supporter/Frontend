b2f9ebc2421e116aeba47844e9ffa62a2251d674
b2etr <102886338+betterb2etr@users.noreply.github.com>
2023-10-11 20:42:18 +0900

    chacinfo js changing
    changing

--- chacinfo.js
+++ chacinfo.js
@@ -1,32 +1,35 @@
 window.onload=()=>{
 
     //panel-faq-container
-    const panelFaqContainer = document.querySelectorAll('.panel-faq-container');
-    console.log(panelFaqContainer);//NodeList객체
+    const panelContainer = document.querySelectorAll('.panel-container');
+    console.log(panelContainer);//NodeList객체
 
     // panel-faq-answer
-    let panelFaqAnswer = document.querySelectorAll('.panel-faq-answer');
-    console.log(panelFaqAnswer);
+    let panelAnswer = document.querySelectorAll('.panel-answer');
+    console.log(panelAnswer);
 
     //btn-all-close
     const btnAllClose=document.getElementById('btn-all-close');
     console.log(btnAllClose);
 
 
+
     //반복문 순회하면서 해당FAQ제목 클릭시 콜백 처리
-    for(let i=0;i<panelFaqContainer.length;i++){
-        panelFaqContainer[i].addEventListener('click',function(){
+    for(let i=0;i<panelContainer.length;i++){
+        panelContainer[i].addEventListener('click',function(){
             //클릭시 처리할일
-            console.log('나 클릭...' + i);   
+            console.log('나 클릭...' + i);  
 
             //FAQ제목클릭시 > 본문이 보이게끔 > active 클래스 추가
-            //this.classList.add('active');
-            panelFaqAnswer[i].classList.add('active');
+            panelAnswer[i].classList.add('active');
         });
     }
     btnAllClose.addEventListener('click',function(){
-        //버튼클릭시 처리할일
-      //  let panelFaqAnswer=document.querySelectorAll('.panel-faq-answer')
+    console.log('all close button click');
 
+    //버튼클릭시 처리할일
+    for(let i=0;i<panelAnswer.length;i++){
+        panelAnswer[i].classList.remove('active');
+    }
     });
 }
