a08f132bac446f8f7b303d1e369d072ff60fe028
b2etr <102886338+betterb2etr@users.noreply.github.com>
2023-11-11 02:22:31 +0900

    changing
    changing

--- ranking.js
+++ ranking.js
@@ -3,6 +3,7 @@
   //var jwt = localStorage.getItem("jwt");
  var jwt="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidGVzdCIsImlkIjoidGVzdCIsImlhdCI6MTY5OTYzMjA1NywiZXhwIjoxNzAyMjI0MDU3fQ.P0AX2jyS18tjHFKBQRy6Wi6OWG9AlDQ4hJtpZp3woB0";
 
+<<<<<<< Updated upstream
   if (jwt == null) {
    window.location.href = './ranking.html'
  }
@@ -52,3 +53,50 @@
  
  
    
+=======
+    // 데이터가 도착할 때 실행할 콜백 함수 설정
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
+            var data = JSON.parse(xhttp.responseText); // 서버에서 받은 데이터 파싱
+            console.log(data);
+
+            var singlePlayer = data[0];
+            displayPlayerInfo(singlePlayer);
+            displayData(data); // 데이터를 HTML에 표시하는 함수 호출
+        }
+    };
+
+    // 요청 보내기
+    xhttp.send();
+});
+
+
+function displayPlayerInfo(player) {
+    var playerInfoContainer = document.querySelector('.champion'); // 클래스로 선택
+
+    var playerElement = document.createElement('div');
+    playerElement.innerHTML = `
+        <p>이름: ${player.name}</p>
+        <p>티어: ${player.tier}</p>
+    `;
+
+    playerInfoContainer.appendChild(playerElement);
+}
+function displayData(data) {
+
+  // 플레이어 목록을 표현할 부분
+  var playerListContainer = document.querySelector('.under10th'); // 클래스로 선택
+
+  // 배열 데이터를 동적으로 HTML에 추가
+  data.forEach(player => {
+      var playerElement = document.createElement('div');
+      playerElement.innerHTML = `
+          <p>이름: ${player.name}</p>
+          <p>티어: ${player.tier}</p>
+          <hr>
+      `;
+      playerListContainer.appendChild(playerElement);
+  });
+}
+
+>>>>>>> Stashed changes
