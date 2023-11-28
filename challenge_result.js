c32416ca45cce666a8761608894ed8943a37a2bd
NEWJIN <winniepooh00@naver.com>
2023-11-29 02:41:01 +0900

    feat: challenge 미확인 신청, 전적 기능 구현
    ....

--- challenge_result.js
+++ challenge_result.js
@@ -2,10 +2,52 @@ const wrapper3 = document.querySelector('.wrapper_result')
 const btnPopup3 = document.querySelector('.btnResult-popup');
 const iconClose3 = document.querySelector('.icon-close3');
 
-btnPopup3.addEventListener('click', () => { wrapper3.classList.add('active-popup'); });
-iconClose3.addEventListener('click', () => { wrapper3.classList.remove('active-popup'); });
+var jwt = localStorage.getItem("jwt");
 
+// challengess 배열 초기화 (서버에서 불러온 데이터를 여기에 저장)
+let challengess = [];
+
+// 대결 전적 정보 연결
+document.addEventListener("DOMContentLoaded", loadName);
 document.addEventListener("DOMContentLoaded", loadChallengeInfo);
+
+function loadName() {
+    const userId = localStorage.getItem("userId");
+    jwt = localStorage.getItem("jwt");
+
+    console.log("UserID from localStorage:", userId);
+    console.log("JWT from localStorage:", jwt);
+
+    if (!userId || !jwt) {
+        console.error("No userID or JWT found in localStorage");
+        return;
+    }
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", `http://34.127.90.191:3000/user/info/${userId}`); // 본인 이름 띄우기
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE) {
+            if (this.status >= 200 && this.status < 300) {
+                try {
+                    const objects = JSON.parse(this.responseText);
+                    console.log("Response about user:", objects);
+
+                    if (objects["status"] == "ok") {
+                        document.getElementById("contender_result").textContent = objects["name"];
+                    }
+                } catch (e) {
+                    console.error("Error parsing response:", e);
+                }
+            } else {
+                console.error("Server responded with status:", this.status);
+            }
+        }
+    };
+}
 
 function loadChallengeInfo() {
     const userId = localStorage.getItem("userId");
@@ -17,7 +59,7 @@ function loadChallengeInfo() {
     }
 
     const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", `http://34.127.90.191:3000/challenge/check/${userId}`); // 신청 정보 띄우기
+    xhttp.open("GET", `http://34.127.90.191:3000/user/match/${userId}`); // 신청 정보 띄우기
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.setRequestHeader("Authorization", "Bearer " + jwt);
     xhttp.send();
@@ -29,20 +71,22 @@ function loadChallengeInfo() {
                     const response = JSON.parse(this.responseText);
                     console.log("Response from challenge check:", response);
 
-                    // challengelist가 존재하는지 확인
-                    if (response.challengelist && Array.isArray(response.challengelist)) {
-                        const listElement = document.querySelector(".board_list_ur");
-                        response.challengelist.forEach((challenge, index) => {
+                    // matchlist가 존재하는지 확인
+                    if (response.matchlist && Array.isArray(response.matchlist)) {
+                        challengess = response.matchlist; // 서버로부터 받은 데이터를 challengess 배열에 저장
+                        const listElement = document.querySelector(".board_list_p");
+                        response.matchlist.forEach((challenge, index) => {
                             const challengeRow = document.createElement("div");
                             challengeRow.innerHTML = `
                                 <div class="num">${index + 1}</div>
-                                <div class="name"><button class="btnAccept-popup">${challenge.challenger}</button></div>
-                                <div class="date">${new Date(challenge.creationDate).toLocaleDateString()}</div>
+                                <div class="name"><button class="btnResult-popup">${challenge.challenger}</button></div>
+                                <div class="result">${challenge.winner}</div>
+                                <div class="date">${new Date(challenge.matchDate).toLocaleDateString()}</div>
                             `;
                             listElement.appendChild(challengeRow);
                         });
                     } else {
-                        console.error("challengelist is not an array or undefined");
+                        console.error("matchlist is not an array or undefined");
                     }
 
                 } catch (e) {
@@ -54,76 +98,27 @@ function loadChallengeInfo() {
         }
     };
 }
-/*
-function result() {
-    const challenger = document.getElementById("challenger_result").value;
-    const contender = document.getElementById("contender_result").value;
-    const score_challenger = document.getElementById("result_challenger").value;
-    const score_contender = document.getElementById("result_contender").value;
 
-  
-    const userId = localStorage.getItem("userId");
-    jwt = localStorage.getItem("jwt");
-  
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", `http://34.127.90.191:3000/challenge/result`);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  
-    xhttp.send(
-      JSON.stringify({
-        challenge_id: userId,
-        challenger: challenger,
-        contender: contender,
-        score_challenger: score_challenger,
-        score_contender: score_contender
-      })
-    );
-  
-    xhttp.onreadystatechange = function () {
-      if (this.readyState === XMLHttpRequest.DONE) {
-        try {
-          if (this.status >= 200 && this.status < 300) {
-            const objects = JSON.parse(this.responseText); //서버로부터 받은 응답텍스트를 JSON 객체로 변환
-            console.log(objects); //서버로부터 받은 응답 콘솔에 출력
-  
-            if (objects["status"] === "ok") { //서버로부터 받은 응답의 상태가 ok일 때
-              localStorage.setItem("jwt", objects["token"]);
-              Swal.fire({
-                text: "결과가 티어에 반영되었습니다!",
-                icon: "success",
-                confirmButtonText: "OK", //성공 메시지를 사용자에게 보여줌
-              }).then((result) => { //확인 버튼 누르면
-                if (result.isConfirmed) {
-                  window.location.href = "./myinfo.html"; //해당 페이지로 리다이렉션
-                }
-              });
-            } else {
-              Swal.fire({
-                text: "결과 반영에 실패했습니다.",
-                icon: "error",
-                confirmButtonText: "OK",
-              });
-              console.error("Empty response received from the server");
-            }
-          } else {
-            Swal.fire({
-              text: "서버 오류 발생. 잠시 후 다시 시도해주세요.",
-              icon: "error",
-              confirmButtonText: "OK",
-            });
-            console.error("Server responded with status:", this.status);
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('click', function (event) {
+      // 클릭된 요소가 btnResult-popup 클래스를 가지고 있는지 확인
+      if (event.target && event.target.classList.contains('btnResult-popup')) {
+          // 해당 버튼과 관련된 challenge 객체 찾기
+          const challenge = challengess.find(ch => ch.challenger === event.target.textContent);
+          if (challenge) {
+              // challenge 객체에서 정보 가져와서 설정하기
+              document.getElementById("challenger_result").textContent = challenge.challenger;
+              document.getElementById("date_result").textContent = new Date(challenge.matchDate).toLocaleDateString();
+              document.getElementById("result_winner").textContent = challenge.winscore;
+              document.getElementById("result_loser").textContent = challenge.losescore;
           }
-        } catch (e) {
-          Swal.fire({
-            text: "응답 처리 중 오류가 발생했습니다.",
-            icon: "error",
-            confirmButtonText: "OK",
-          });
-          console.error("Error parsing response:", e);
-        }
+          wrapper3.classList.add('active-popup');
+          currentSelectedChallenge = challenge;
       }
-    };
-    return false;
-  }
+  });
 
-*/
+  const iconClose3 = document.querySelector('.icon-close3');
+  iconClose3.addEventListener('click', () => {
+      wrapper3.classList.remove('active-popup');
+  });
+});
